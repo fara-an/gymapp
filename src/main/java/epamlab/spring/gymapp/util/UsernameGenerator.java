e3d6a956ae8b8a13901e1dd1024aa19e4b951ada
yodgora <anorayodgora@gmail.com>
2025-06-06 12:59:12 +0500

    fixed typo

--- src/main/java/epamlab/spring/gymapp/util/UsernameGenerator.java
+++ src/main/java/epamlab/spring/gymapp/util/UsernameGenerator.java
@@ -6,13 +6,13 @@ public class UsernameGenerator {
 
 
 
-    public  static String generateUsername(String firstName, String lastName, boolean usernameExits ) {
+    public  static String generateUsername(String firstName, String lastName, boolean usernameExists ) {
 
         String baseUsername = firstName + "." + lastName;
         String username = baseUsername;
         int suffix = 1;
 
-        while (usernameExits) {
+        while (usernameExists) {
             username = baseUsername + suffix;
             suffix++;
         }
