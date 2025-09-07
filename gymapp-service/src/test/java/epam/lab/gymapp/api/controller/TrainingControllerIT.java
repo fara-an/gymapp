859ac815bf44eb14ab5b7c962547761b3248dfdd
yodgora <anorayodgora@gmail.com>
2025-09-07 17:40:54 +0500

    got dependencies for integ testing

---
+++ gymapp-service/src/test/java/epam/lab/gymapp/api/controller/TrainingControllerIT.java
@@ -0,0 +1,14 @@
+package epam.lab.gymapp.api.controller;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Testcontainers
+@SpringBootTest
+@AutoConfigureTestDatabase
+
+class TrainingControllerIT {
+
+}
